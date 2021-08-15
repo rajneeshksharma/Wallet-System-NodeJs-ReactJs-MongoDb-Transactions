14deb71bdcc6ac4acc40d7d0a7f319f1e85acd3e
rajneeshksharma <rajneeshksharma40@gmail.com>
2021-08-16 00:25:16 +0530

    Build Fix for url added

--- test-frontend/src/helper/Api.js
+++ test-frontend/src/helper/Api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: `http://localhost:3001/api/v1`
-  // baseURL: `${window.location.protocol}//${window.location.host}/api/v1`
+  // baseURL: `http://localhost:3001/api/v1`
+  baseURL: `${window.location.protocol}//${window.location.host}/api/v1`
 });
